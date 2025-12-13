63fa80dde6ced2ddc390d5954245be3f517b2733
Marcio <putheworkin@gmail.com>
2025-12-13 14:38:35 +0000

    feat(app): add App Router pages for /trending and /ask-novus

---
+++ app/ask-novus/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+import AskNovus from "../../components/AskNovus";
+
+export default function AskNovusPage() {
+  return (
+    <main className="min-h-screen p-6">
+      <AskNovus onSearch={() => {}} />
+    </main>
+  );
+}
---
+++ app/trending/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+import GlobalTrending from "../../components/GlobalTrending";
+
+export default function TrendingPage() {
+  return (
+    <main className="min-h-screen p-6">
+      <GlobalTrending />
+    </main>
+  );
+}
