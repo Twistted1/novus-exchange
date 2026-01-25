3a30717f1c06e33c7149b9880bfe6ea22767b5c1
Marcio <putheworkin@gmail.com>
2026-01-25 15:55:56 +0000

    Security fix: Remove hardcoded secrets and stop tracking .env.local

--- src/components/lib/firebase.js
+++ src/components/lib/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD8BNHHUFfkCGJRUN-VHPH0llZaFKgJ5qY",
-  authDomain: "novus-exchange-cms.firebaseapp.com",
-  projectId: "novus-exchange-cms",
-  storageBucket: "novus-exchange-cms.firebasestorage.app",
-  messagingSenderId: "103100897243",
-  appId: "1:103100897243:web:77aba09fcb9e784d0bf9fe",
-  measurementId: "G-HBW88BWZSK"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
